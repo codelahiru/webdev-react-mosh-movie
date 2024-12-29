b3ba15ab19779028d6ca56f534ecf2141b839cd7
codehaku <codelahiru@gmail.com>
2024-12-29 09:25:30 +0530

    initial commit

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+
+// PascalCasing - always folow PascalCasing when creating functions
+function Message(){
+    // the htnl code you see below which is written inside a javascript code 
+    // is called JSX: JavaScript XML
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
